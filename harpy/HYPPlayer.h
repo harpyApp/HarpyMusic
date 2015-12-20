2e426cc125e4dbe6fde56f901a510e184f5132f4
kiaraRobles <kiara.robles@gmail.com>
2015-12-20 10:01:14 -0500

    Added player tab to tab view contoller

---
+++ harpy/HYPPlayer.h
@@ -0,0 +1,13 @@
+//
+//  HYPPlayer.h
+//  harpy
+//
+//  Created by Kiara Robles on 12/20/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYPPlayer : UIViewController
+
+@end
---
+++ harpy/HYPPlayer.m
@@ -0,0 +1,13 @@
+//
+//  HYPPlayer.m
+//  harpy
+//
+//  Created by Kiara Robles on 12/20/15.
+//  Copyright © 2015 teamFloppyDisk. All rights reserved.
+//
+
+#import "HYPPlayer.h"
+
+@implementation HYPPlayer
+
+@end
